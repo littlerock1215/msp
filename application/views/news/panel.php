1027eef770c2bd768e9411c03cf900d5cf95f1ce
Rock <littlerock1215@hotmail.com>
2019-11-12 13:56:49 +0800

    news page css fix

--- application/views/news/panel.php
+++ application/views/news/panel.php
@@ -8,6 +8,18 @@
 </div>
 
 <div class="content_block">
-    
-
+    <ul>
+        <li>
+            <a href="#">2019.09.31
+                <span class="content_title">徵人啟事</span>
+                <span class="news_arrow"></span>
+            </a>
+        </li>
+        <li>
+            <a href="#">2019.05.17
+                <span class="content_title">EDS電控說明書</span>
+                <span class="news_arrow"></span>
+            </a>
+        </li>
+    </ul>
 </div>
