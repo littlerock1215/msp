09fbf8d8cf762b5d949d00ebdf65b6d24b8ae3ee
s5218qaz <a0905078881@gmail.com>
2019-11-12 16:43:02 +0800

    qa promote mobile

--- application/views/QA/panel.php
+++ application/views/QA/panel.php
@@ -2,7 +2,7 @@
 
 <div class="title_block">
     <div class="content_title">
-        <a href="#">主頁</a><span>></span><a href="#">Q&A</a><span style="color:black;">></span><a href="#">購車</a>
+        <a href="#">主頁</a><span>></span><a href="#">Q&A</a>
         <h1 class="page_title">Q&A</h1>
     </div>
 </div>
@@ -114,21 +114,101 @@
 
 
     <div class="mobile_qa_block">
-        <ul class="menu">
+        <ul class="menu_mobile">
             <li><a  href="" data-target="sell_car">購車<i class="mobile_fal fal fa-angle-right"></i></a>
-                <div style="display:none;" class="sell_car_block">1</div>
+                <div style="display:none;" class="sell_car_block">
+                    <ul>
+                        <li class="click_item">
+                            <div class="question"> Q. 購車?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+
+                        <li class="click_item">
+                            <div class="question"> Q. MSP主要業務內容為?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+
+                        <li class="click_item">
+                            <div class="question"> Q. MSP主要業務內容為?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+                    </ul>
+                </div>
             </li>
                 
             <li><a href="" data-target="after_sell">售後服務<i class="mobile_fal fal fa-angle-right"></i></a>
-                <div style="display:none;" class="after_sell_block">12</div>
+                <div style="display:none;" class="after_sell_block">
+                    <ul>
+                        <li class="click_item">
+                            <div class="question"> Q. 售後服務?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+
+                        <li class="click_item">
+                            <div class="question"> Q. MSP主要業務內容為?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+
+                        <li class="click_item">
+                            <div class="question"> Q. MSP主要業務內容為?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+                    </ul>
+                </div>
             </li>
 
-            <li><a href="" data-target="about" class="active">零件相關<i class="mobile_fal fal fa-angle-right"></i></a>
-                <div style="display:none;" class="about_block">31</div>
+            <li><a href="" data-target="about">零件相關<i class="mobile_fal fal fa-angle-right"></i></a>
+                <div style="display:none;" class="about_block">
+                    <ul>
+                        <li class="click_item">
+                            <div class="question"> Q. 零件相關?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+
+                        <li class="click_item">
+                            <div class="question"> Q. MSP主要業務內容為?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+
+                        <li class="click_item">
+                            <div class="question"> Q. MSP主要業務內容為?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+                    </ul>
+                </div>
             </li>
 
             <li><a href="" data-target="other_qas">其他問題<i class="mobile_fal fal fa-angle-right"></i></a>
-                <div style="display:none;" class="other_qas_block">e1</div>
+                <div style="display:none;" class="other_qas_block">
+                    <ul>
+                        <li class="click_item">
+                            <div class="question"> Q. 其他問題?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+
+                        <li class="click_item">
+                            <div class="question"> Q. MSP主要業務內容為?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+
+                        <li class="click_item">
+                            <div class="question"> Q. MSP主要業務內容為?</div>
+                            <button class="answer_button"></button>
+                            <div class="answer">A. 初期以「速克達」機車賽事用之排氣管為主力產品，後期研發製造高性能避震器</div>
+                        </li>
+                    </ul>
+                </div>
             </li>        
         </ul>
     
@@ -143,15 +223,29 @@ $(document).ready(function(){
     $( '.click_item' ).click(function() {
         if($(".answer_button").hasClass('open')){
             $(".answer_button").removeClass('open');
-            $(".click_item").closest('li').children('.answer').toggle();
+            $(".click_item").children('.answer').toggle();
 
         }else{
             $(".answer_button").addClass('open');
-            $(".answer_button").closest('li').children('.answer').toggle();
+            $(".answer_button").children('.answer').toggle();
         }
     });
 
     $('.menu a').click(function(e){
+        e.preventDefault();
+        var target = $(this).data('target');
+
+        if(!$(this).hasClass('active')){
+            var now = $('.menu a.active').data('target');
+            $('.menu a.active').removeClass('active');
+            $('.'+now+'_block').hide();
+            $('.'+target+'_block').fadeIn();
+            $(this).addClass('active');
+        }
+    });
+
+
+    $('.menu_mobile a').click(function(e){
         e.preventDefault();
         var target = $(this).data('target');
 
@@ -165,7 +259,7 @@ $(document).ready(function(){
             var now = $('.menu a.active').data('target');
             $('.'+now+'_block').hide();
             $('a[data-target='+now+'] > i').removeClass('fa-angle-up').addClass('fa-angle-right');
-            
+
             $('.'+target+'_block').fadeIn();
             $(this).addClass('active');
         }else{
